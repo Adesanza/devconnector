b7ca7bb3064943fa71ee80deec47de3f60b93144
Adeshina <badmusadeshinna@gmail.com>
2021-02-24 17:08:37 +0100

    setting up home route

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running yey!'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is live @ ${PORT}`));

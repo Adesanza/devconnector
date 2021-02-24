902aaff76443769d54c3224d66f50113d0305420
Adeshina <badmusadeshinna@gmail.com>
2021-02-24 18:33:11 +0100

    before i start request and body validation

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongooose = require('mongoose');
+
+const UserSchema = new mongooose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);

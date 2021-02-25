bfa957e2d1ebe3fed263d4f3bd2c30ff5e837695
Adeshina <badmusadeshinna@gmail.com>
2021-02-25 13:27:23 +0100

    getting all users by their Id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,121 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => res.send('profile route'));
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server went Bad");
+  }
+});
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        Instagram,
+        Linkedin
+    } = req.body;
 
 
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+    if(skills){
+        profileFields.skills = skills.split(',').map(skills => skills.trim());
+    }
+
+    profileFields.social = {}
+    if(youtube) profileFields.social.youtube = youtube;
+    if(twitter) profileFields.social.twitter = twitter;
+    if(facebook) profileFields.social.facebook = facebook;
+    if(Linkedin) profileFields.social.Linkedin = Linkedin;
+    if(Instagram) profileFields.social.Instagram = Instagram;
+
+   try{
+    let profile = await Profile.findOne({user: req.user.id});
+
+    if(profile){
+        profile = await Profile.findOneAndUpdate({user: req.user.id}, {$set: profileFields}, {new: true});
+        return res.json(profile);
+    }
+
+    profile = new Profile(profileFields);
+
+    await profile.save();
+    res.json(profile);
+   }catch(err){
+       console.error(err.message);
+       res.status(500).send('Server went Bad')
+   }
+  }
+);
+
+router.get('/', async(req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+            res.status(500).send('Server went bad')
+    }
+});
+
+// GET API/PROFILE/USER_ID {ROUT}///// 
+
+router.get('/user/:user_id', async(req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar']);
+        
+        if(!profile) return res.status(400).json({msg: 'No Profile '});
+
+    return res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: "No Profile "});
+        }
+            res.status(500).send('Server went bad')
+    }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,76 +1,75 @@
-const express = require('express');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
-const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config')
-const User = require('../../models/User');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("../../models/User");
 
-router.post('/',
-    check('name', 'Name is required').notEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check(
-        'password',
-        'Please enter a password with 6 or more characters'
-    ).isLength({min: 6})
-, async (req, res) => {
+router.post(
+  "/",
+  check("name", "Name is required").notEmpty(),
+  check("email", "Please enter a valid email").isEmail(),
+  check(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 }),
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
-    try{
-        let user = await User.findOne({ email});
-        if(user){
-           return res.status(400).json({errors: [{msg: 'User already exists'}] });
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("mysecrettoken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
-
-        const avatar = gravatar.url(email, {
-            s: "200",
-            r: "pg",
-            d: "mm"
-        })
-
-        user = new User({
-            name,
-            email,
-            avatar,
-            password
-        });
-
-        const salt = await bcrypt.genSalt(10);
-
-        user.password = await bcrypt.hash(password, salt);
-
-        await user.save();
-
-
-        const payload ={
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign
-        (payload, 
-        config.get('mysecrettoken'),
-        {expiresIn: 360000},
-        (err, token) =>{
-            if(err) throw err;
-            res.json({ token });
-        });
-    }catch(err){
-        console.error(err.mesage);
-        res.status(500).send('Server error')
+      );
+    } catch (err) {
+      console.error(err.mesage);
+      res.status(500).send("Server error");
     }
-
- 
-    
-});
-
-
+  }
+);
 
 module.exports = router;

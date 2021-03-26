ddc1b2585b24f8963e7e1f341801d114b3679ce6
Adeshina <badmusadeshinna@gmail.com>
2021-03-26 13:54:05 +0100

    router for landing and nav react

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+      <Navbar />
+      <Route exact path="/" component={ Landing } />
+      {/* <Landing /> */}
+      </Fragment>
+    </Router>
   );
 }
 

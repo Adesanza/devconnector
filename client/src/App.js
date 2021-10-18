9b56f067eb0002874561185d2b2310f4c5d7dda5
Badmus Adeshina <badmusadeshinna@gmail.com>
2021-10-18 11:24:34 +0100

    setAlert issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-        {/* <Landing /> */}
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+          {/* <Landing /> */}
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 export default App;
